e4bd2f40108e714a2470876527c72c954a472e0b
syrine <syrine.ounais@esprit.tn>
2023-11-22 11:54:57 +0100

    integration gestion des employes et vehicule electrique

---
+++ authentification.h
@@ -0,0 +1,29 @@
+#ifndef AUTHENTIFICATION_H
+#define AUTHENTIFICATION_H
+
+#include <QDialog>
+#include "gestion_employes.h"
+
+namespace Ui {
+class Authentification;
+}
+
+class Authentification : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Authentification(QWidget *parent = nullptr);
+    ~Authentification();
+
+private slots:
+    void on_login_clicked();
+
+    bool authenticateEmployee(const QString &email, const QString &password);
+
+private:
+    Ui::Authentification *ui;
+    gestion_employes w;
+};
+
+#endif // AUTHENTIFICATION_H
